28c431cd65c27eeb8fc75f20c3b2abe3e28b1417
kishore-devaraj <kishoregrylls@gmail.com>
2018-10-23 14:12:49 +0530

    Complete workflow of handcricket has been added

--- app.js
+++ app.js
@@ -9,14 +9,59 @@ if (!isInputValid(toss)) return
 handCricket.tossWon = handCricket.toss() === toss ? true : false
 if (handCricket.tossWon) {
     console.log('Toss Won')
-    const chooseTo = Number(readlineSync.question('\'Batting\' or \'Bowling\' ? Choose either 0 or 1: '))
-    
+    let chooseTo = Number(readlineSync.question('\'Batting\' or \'Bowling\' ? Choose either 0 or 1: '))
+
     if (!isInputValid(chooseTo)) return
-    handCricket.teamOne.currentStatus = [Number(chooseTo)] === 0 ? 'Bowling' : 'Batting'
-    console.log('You have chosen to ' + handCricket.teamOne.currentStatus)
+    chooseTo = chooseTo === 0 ? 'Batting' : 'Bowling'
+    handCricket.battingTeam = chooseTo === 'Batting' ? handCricket.teamOne : handCricket.teamTwo
+    handCricket.bowlingTeam = chooseTo === 'Batting' ? handCricket.teamTwo : handCricket.teamOne
+
+    console.log('You have chosen to ' + chooseTo)
 } else {
     console.log('Toss Lost')
-    console.log('Computer choose to: ' + handCricket.teamTwo.teamChooseTo())
+    const computerChooseTo = handCricket.teamTwo.teamChooseTo()
+    handCricket.battingTeam = computerChooseTo === 'Batting' ? handCricket.teamTwo : handCricket.teamOne
+    handCricket.bowlingTeam = computerChooseTo === 'Batting' ? handCricket.teamOne : handCricket.teamTwo
+    console.log('Computer choose to: ' + computerChooseTo)
+}
+
+
+let playCricket = true
+while (playCricket) {
+    console.log('*****************************************************************')
+    console.log(`${handCricket.battingTeam.teamName} is Batting!`)
+    // console.log(`${handCricket.battingTeam}`)
+    // console.log(typeof handCricket.battingTeam.remainingBalls)
+
+    while (handCricket.battingTeam.remainingBalls !== 0) {
+        console.log(`${handCricket.battingTeam.remainingBalls} balls remaining`)
+        console.log(`Score: ${handCricket.battingTeam.score}`)
+        const userInput = getInputFromUser()
+        if (handCricket.check(userInput)) {
+            handCricket.battingTeam === handCricket.teamOne ? console.log('You are OUT!')
+                : console.log('You have taken the wicket')
+            handCricket.battingTeam.remainingBalls = 0
+            break
+        } else {
+            const score = userInput + handCricket.teamTwo.currentNumber
+            handCricket.battingTeam.score += score
+            handCricket.battingTeam.remainingBalls = handCricket.battingTeam.remainingBalls - 1
+        }
+
+        // Checking whether the match is ended and declaring the winner
+        if (handCricket.didMatchEnded()) {
+            const winner = handCricket.getWinner()
+            if (!winner) console.log('It\'s is Tie')
+            else console.log(`${winner.teamName} is Winner!`)
+        }
+    }
+
+    console.log(`${handCricket.battingTeam.teamName}\'s Final Score: ${handCricket.battingTeam.score}`)
+    handCricket.battingTeam = handCricket.battingTeam === handCricket.teamOne ? handCricket.teamTwo : handCricket.teamOne
+    handCricket.battingTeam = handCricket.battingTeam === handCricket.teamOne ? handCricket.teamOne : handCricket.teamTwo
+
+    playCricket = handCricket.teamOne.remainingBalls === 0 && handCricket.teamTwo.remainingBalls === 0 ?
+        false : true
 }
 
 
@@ -28,6 +73,16 @@ function isInputValid(input) {
         return false
     }
     return true
+}
+
+function getInputFromUser() {
+    const input = Number(readlineSync.question('Enter the number from 1 - 6? :'))
+    if ([1, 2, 3, 4, 5, 6].indexOf(input) !== -1) {
+        return input
+    } else {
+        console.log('Invalid input, Try again')
+        getInputFromUser()
+    }
 }
 
 
--- models.js
+++ models.js
@@ -1,35 +1,63 @@
 'use strict'
 
-function Player (name) {
+const readlineSync = require('readline-sync')
+
+function Player(name) {
     this.name = name
     this.score = 0
     this.ballsFaced = 0
 }
 
-function Team (teamName, teamSize) {
+function Team(teamName, teamSize) {
     this.teamName = teamName
-    this.teamScore = 0
+    this.score = 0
+    this.currentNumber = 0
     this.totalPlayers = teamSize
+    this.remainingBalls = 6
     this.players = new Array(this.totalPlayers)
 }
 
 Team.prototype.teamChooseTo = function () {
     const options = ['Batting', 'Bowling']
-    this.currentStatus =  options[Math.floor(Math.random() * options.length)]
+    this.currentStatus = options[Math.floor(Math.random() * options.length)]
     return this.currentStatus
 }
 
-function HandCricket (teamOneName) {
+function HandCricket(teamOneName) {
     this.teamOne = new Team(teamOneName, 1)
     this.teamTwo = new Team('Computer Team', 1)
     this.teamOneScore = 0
     this.teamTwoScore = 0
-    this.teamOneBallsRemaining = 6
-    this.teamTwoBallsRemaining = 6
+    this.battingTeam = null
+    this.bowlingTeam = null
+}
+HandCricket.prototype.didMatchEnded = function () {
+    if (!(this.battingTeam.remainingBalls < 6 && this.bowlingTeam.remainingBalls < 6)) return false
+    else if (this.battingTeam.score > this.bowlingTeam.score) return true
+    else return false
+}
+
+HandCricket.prototype.getWinner = function () {
+    if (this.battingTeam.score === this.bowlingTeam.score) return false
+    else {
+        if (this.battingTeam.score > this.bowlingTeam.score) return this.battingTeam
+        else return this.bowlingTeam
+    }
+}
+
+HandCricket.prototype.check = function (number) {
+    const computerNumber = this.getComputerNumber()
+    console.log('Computer number is ' + computerNumber)
+    this.teamTwo.currentNumber = computerNumber
+    return computerNumber === number
 }
 
 HandCricket.prototype.toss = function () {
     return Math.floor(Math.random() * Math.floor(2))
+}
+
+HandCricket.prototype.getComputerNumber = function () {
+    return Math.floor(Math.random() * Math.floor(6)) + 1
 }
 
 
